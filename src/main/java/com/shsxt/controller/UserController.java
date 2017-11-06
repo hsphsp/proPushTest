0670629110bdc46297bfe6ec80d79776d69c7e4f
hsphsp <pinghsp31@163.com>
2017-11-06 20:48:22 +0800

    新建一个controller

---
+++ src/main/java/com/shsxt/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.shsxt.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by asus on 2017/11/6.
+ */
+@WebServlet(urlPatterns = "/user")
+public class UserController extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("hello push to gitHub");
+    }
+}
